8960219609d1b934328ddbce75dd009f85a216b5
TheFriendlyGhost <ncaspar302@gmail.com>
2018-07-03 17:59:41 -0700

    even more changes

--- controller/app_controller.js
+++ controller/app_controller.js
@@ -1,38 +1,33 @@
 var Article = require('../models/Article');
 var Note = require('../models/Note');
 var cheerio = require('cheerio');
-var axios = require("axios")
+var request = require('request');
 
 
 module.exports = function(app){
 	app.get("/scrape", function(req, res) {
-	  axios.get("https://www.nytimes.com/").then(function(response) {
-	    var $ = cheerio.load(response.data);
+	  request("https://www.nytimes.com/"), function(_error, _response, html) {
+	    var $ = cheerio.load(html);
 
 	    $(".story-heading").each(function(i, element) {
-	      // Save an empty result object
 	      var result = {};
 
-	      // Add the text and href of every link, and save them as properties of the result object
 	      result.title = $(this).children("a").text();
 	      result.link = $(this).children("a").attr("href")
 	      result.summary = $(this).siblings(".summary").text().trim()
 
-	      // Create a new Article using the `result` object built from scraping
 	      Article.create(result)
 	        .then(function(dbArticle) {
-	          // View the added result in the console
 	          console.log(dbArticle);
 	        })
 	        .catch(function(err) {
-	          // If an error occurred, send it to the client
 	          return res.json(err);
 	        });
 	    });
 
 	    // If we were able to successfully scrape and save an Article, send a message to the client
 	    res.send("Scrape Complete");
-	  });
+	  };
 	});
 
 	// Route for getting all Articles from the db
--- server.js
+++ server.js
@@ -20,7 +20,6 @@ app.use(bodyParser.urlencoded({
 // Connect to the Mongo DB
 // mongoose.connect("mongodb://localhost/MongoDBHW");
 mongoose.connect("mongodb://heroku_mddfp0f2:tWPlvar0SvpwqqVorhICuzhS3EUhiq6T@ds125831.mlab.com:25831/heroku_mddfp0f2");
-
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
